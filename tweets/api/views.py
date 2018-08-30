f5a9371bca4e2c763f494656920e6d96a8661b02
Guangchu Shi <rollschild@gmail.com>
2018-08-30 13:15:52 -0400

    Added jQuery search functionality

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-
+from django.db.models import Q
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 
@@ -7,5 +7,11 @@ from .serializers import TweetModelSerializer
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
 
-    def get_queryset(self):
-        return Tweet.objects.all()
+    def get_queryset(self, *args, **kwargs):
+        queryset = Tweet.objects.all()
+        print(self.request.GET)
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            queryset = queryset.filter(Q(content__icontains=query)
+                                       | Q(user__username__icontains=query))
+        return queryset
