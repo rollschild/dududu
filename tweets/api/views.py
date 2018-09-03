4baf892ad8e6d56b5fc4c2cb1d550a147144ccdd
Guangchu Shi <rollschild@gmail.com>
2018-09-03 11:03:29 -0400

    Added pagination and load more functionality

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 from rest_framework import permissions
+from .pagination import StandardResultsPagination
 
 
 class TweetCreateAPIView(generics.CreateAPIView):
@@ -15,6 +16,7 @@ class TweetCreateAPIView(generics.CreateAPIView):
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
+    pagination_class = StandardResultsPagination
 
     def get_queryset(self, *args, **kwargs):
         queryset = Tweet.objects.all().order_by("-timestamp")
