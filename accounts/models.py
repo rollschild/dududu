016eafbe015d490f7dc482bc12580ec7e838c0ad
Guangchu Shi <rollschild@gmail.com>
2018-09-05 10:53:35 -0400

    Added signals to dispatch user profile changes

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.urls import reverse_lazy
+from django.db.models.signals import post_save
 # Create your models here.
 
 
@@ -70,3 +71,13 @@ class UserProfile(models.Model):
     def get_absolute_url(self):
         return reverse_lazy("profiles:detail", kwargs={
                             "username": self.user.username})
+
+
+def post_save_user_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        # Automatically create user profile for newly add user
+        new_profile = UserProfile.objects.get_or_create(user=instance)
+    print(instance)
+
+
+post_save.connect(post_save_user_receiver, sender=settings.AUTH_USER_MODEL)
